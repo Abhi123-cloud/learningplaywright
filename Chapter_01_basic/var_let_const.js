6a9256ccc9c5c29a1c985d43bd8da2496271595f
Abhilash <you@example.com>
2026-02-14 13:09:09 +0530

    push new code

---
+++ Chapter_01_basic/var_let_const.js
@@ -0,0 +1,13 @@
+
+var a = 10;
+console.log(a);
+
+function add() {
+    var b = 20;
+    console.log(b);
+    if (true) {
+        var b = 30;
+        console.log(c);
+    }
+    console.log(b);
+}
