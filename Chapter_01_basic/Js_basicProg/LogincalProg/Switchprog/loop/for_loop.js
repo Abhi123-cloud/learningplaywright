935df15db28503f23f316fa5d7dd7d4558cfc541
Abhilash <you@example.com>
2026-02-27 08:54:59 +0530

    New code

---
+++ Chapter_01_basic/Js_basicProg/LogincalProg/Switchprog/loop/for_loop.js
@@ -0,0 +1,17 @@
+
+let x = "";
+
+switch (x) {
+    case "chrome":
+        console.log("Open chrome browser");
+        break;
+
+    case "Edge":
+        console.log("Open edge browser");
+        break;
+
+    default:
+        console.log("Open safarii browser");
+        break;
+}
+
